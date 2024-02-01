6c2838c3de5fb53b75fa245daa07b0e357a0c961
tomas marelli <141285997+tomasmarelli@users.noreply.github.com>
2024-02-01 20:41:00 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './style.css';
+import NavBar from './components/header/NavBar';
+
+
+
+
+function App(){
+    return (
+      <NavBar />
+    )
+
+  }
+  export default App;
---
+++ src/components/header/Brand.js
@@ -0,0 +1,10 @@
+import brand from "../../img/brand.svg";
+
+
+const Brand = ()=> {
+	return(
+<img src="{brand)" alt="tienda good" title="tiendita good"></img>
+	)
+}
+
+export default Brand;
